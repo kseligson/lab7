32df06a4665fa0d5ce6f73c9fa2d7f58bff668db
Kaitlin Seligson <kseligso@ucsd.edu>
2016-02-17 02:10:47 +0000

    final analytics lab

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,19 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	/*var random_num = Math.random();
+	console.log(random_num);
+
+	if(random_num > 0.5) {
+		projects["grid"] = false;
+		res.render('index', projects);
+	}
+	else {
+		res.redirect('/grid');
+	}*/
+
 	projects["grid"] = false;
-  	res.render('index', projects);
+	res.render('index', projects);
 };
 
 exports.viewGrid = function(req, res) {
