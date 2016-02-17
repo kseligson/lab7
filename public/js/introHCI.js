f0da8e145c5aed3046206352e68ed8860df5e5c3
Kaitlin Seligson <kseligso@ucsd.edu>
2016-02-17 01:30:21 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,10 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(registerLike);
+}
+
+function registerLike(e) {
+	//e.preventDefault();
+	ga('send','event','like','click');
 }
